a32218ee3929717a6c347b3de7e8409d3ac40be8
Tom van Tilburg <tom.van.tilburg@gmail.com>
2018-03-07 11:05:15 +0100

    flipping cards works

--- elements/geodan-demo.js
+++ elements/geodan-demo.js
@@ -4,6 +4,7 @@ import { repeat } from '../node_modules/lit-html/lib/repeat.js';
 import '../node_modules/@polymer/iron-ajax/iron-ajax.js';
 import '../node_modules/@polymer/paper-input/paper-input.js';
 import '../node_modules/@polymer/paper-item/paper-item.js';
+import '../node_modules/@polymer/paper-card/paper-card.js';
 import '../../node_modules/@polymer/iron-flex-layout/iron-flex-layout-classes.js';
 import '../../node_modules/@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
 import '../../node_modules/@polymer/app-layout/app-drawer/app-drawer.js';
@@ -14,6 +15,102 @@ import '../../node_modules/@polymer/app-layout/app-toolbar/app-toolbar.js';
 class GeodanDemo extends LitElement {
     render({resultaat}){
         return html`
+    <style is="custom-style">
+            :host {
+                display: block;
+                --geodan-regular-font: 'Open Sans Regular';
+                --geodan-light-font: 'Open Sans',sans-serif;
+                --geodan-heading1-fontsize: 36px;
+                --geodan-heading2-fontsize: 24px;
+                --geodan-heading3-fontsize: 17px;
+                --geodan-light-fontweight: 300;
+                --geodan-color: #333333;
+        
+                --geodan-button-normal: #ED3031;
+                --geodan-button-hover:  #D60000;
+                --geodan-button-pressed: #BB0C16;
+                --geodan-button-disabled: #ECECEC;
+                --geodan-button-corner-radius: 4px;
+        
+                --geodan-checkbox-unckeched: #E3E2E3;
+                --geodan-checkbox-checked: #ED3031;
+        
+                --geodan-header-background:  #424242;
+                
+                color: var(--geodan-color);
+            }
+            
+            h1 {
+                font-family: var(--geodan-regular-font);
+                font-size: var(--geodan-heading1-fontsize);
+            }
+            h2 {
+                font-family: var(--geodan-regular-font);
+                font-size: var(--geodan-heading2-fontsize);
+            }
+            h3 {
+                font-family: var(--geodan-regular-font);
+                font-size: var(--geodan-heading3-fontsize);
+            }
+        
+            .title {
+                font-family: var(--geodan-regular-font);
+                font-size: var(--geodan-heading1-fontsize);
+            }
+            paper-button.geodan {
+                background:var(--geodan-button-normal);
+                color:white;
+                border-radius:var(--geodan-button-corner-radius); 
+                --paper-button-ink-color: var(--geodan-button-normal);
+                --paper-button-disabled: var(--geodan-button-disabled);
+            }
+            paper-button.geodan:hover {
+                background:var(--geodan-button-hover);
+            }
+        
+            paper-button.geodan a{
+                color: white;
+            }
+            paper-icon-button {
+                color: var(--geodan-button-normal);
+                --paper-icon-button-ink-color: var(--geodan-button-normal);
+            }
+        
+            a {
+                text-decoration: none;
+                color: var(--geodan-color);
+            }
+        
+            app-header {
+                height: 80px;
+            }
+        
+            app-toolbar {
+                background: var(--geodan-header-background);
+                color: white;
+                --app-toolbar-font-size: 18px;
+                font-family: var(--geodan-light-font);
+                font-weight: var(--geodan-light-fontweight);
+                height: 80px;
+            }
+        
+            paper-tab {
+                --paper-tab-ink: var(--geodan-button-normal);
+            }
+        
+            paper-tabs {
+                --paper-tabs-selection-bar-color: var(--geodan-button-normal);
+        
+            }
+        
+            paper-checkbox {
+                --paper-checkbox-unchecked-color: var(--geodan-checkbox-unchecked);
+                --paper-checkbox-unchecked-ink-color: var(--geodan-checkbox-checked);
+                --paper-checkbox-checked-color: var(--geodan-checkbox-checked);
+                --paper-checkbox-checked-ink-color: var(--geodan-checkbox-unchecked);
+                --paper-checkbox-checkmark-color: white;
+            }
+        </style>    
     <style include="iron-flex iron-flex-alignment">
  
     .demo {
@@ -24,23 +121,37 @@ class GeodanDemo extends LitElement {
         background: white;
         border-radius: 4px;
     }
-    .content {
-        background: white;        
-        padding: 5px; 
-        height: 200px;        
-    }           
-    .content img {
-        max-width: 188px; 
-        max-height: 125px;
-    }   
+    paper-card {
+        
+        perspective: 500px;
+    }
     
+    paper-card:hover .card-content {
+        transform: rotateY( 180deg ) ;
+        transition: transform 0.5s;
+    }
+
     .card-content {
-        overflow: auto;
-        text-decoration: none !important;
+        transition: transform 1s;
+        transform-style: preserve-3d;
     }
-    a.card-content:hover {
-        text-decoration: none !important;
+    .card-content img {
+        height: 100%;
+        width: 100%;
     }
+
+
+    .frontside, .backside {
+        backface-visibility: hidden;
+    }
+    
+    .backside {
+        position: absolute;
+        top: 5px;
+        padding: 5px;
+        transform: rotateY( 180deg );
+    }
+    
     a{
         text-decoration: none ;
         color: #333
@@ -65,12 +176,6 @@ class GeodanDemo extends LitElement {
     .tag {
         margin:5px;
         background: white;
-    }
-    .tag.shadow {
-        padding:3px;
-    }
-    .demoname {
-        font-size: 120%;
     }
     .tags {
         font-size: 80%;
@@ -112,32 +217,31 @@ class GeodanDemo extends LitElement {
     <app-header>
         <app-toolbar>
             <h4 condensed-title="demos">Geodan Research demos</h4>
-
         </app-toolbar>
     </app-header>
 
 
-    <div class="layout horizontal wrap">
+    <div class='layout horizontal wrap'>
     ${repeat(this.resultaat || [], (demo) => demo.title, (demo, index) => html`
-        
-            <a class="demo" href="${demo.url}" style="display:${demo.disabled?"None":"inline-block"}">
-            <div class="content" on-mouseover="${(e)=>demo.hover=true}" on-mouseout="${(e)=>demo.hover=false}">
-                <h2>${demo.title}</h2>
+            <a class="demo" href="${demo.url}" style="display:${demo.disabled?"none":"inline-block"}">
+            <paper-card 
+                heading="" 
+                alt="${demo.title}">
                 <div class="card-content">
-                    <div style="display:${(!demo.hover || !demo.description)?"block":"None"}">
-                        <div style="display:${this.isvideo(demo.thumbnail)?"block":"None"}">
+                    <div class="frontside">
+                        ${this.isvideo(demo.thumbnail)?html`
                             <video src="${demo.thumbnail}" width="100%" height="100%" autoplay="" loop=""></video>
-                        </div>
-                        <div style="display:${this.isvideo(demo.thumbnail)?"None":"block"}">
-                            <img src="${demo.thumbnail}">
-                        </div>
+                        `:html`
+                            <img src="${demo.thumbnail}"/>
+                        `}
+                        <div>${demo.title}</div>
                         <div class="date">${demo.date}</div>
                     </div>
-                    <div style="display:${demo.hover?"block":"None"}">
+                    <div class="backside">
                         ${demo.description}
                     </div>
                 </div>
-            </div>
+            </paper-card>
             
             </a>
         
